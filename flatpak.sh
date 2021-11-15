a5e79fc05229d7f94806205100c9825ccfe2816a
anthonyp <aperullo065@gmail.com>
2021-11-15 00:57:10 -0500

    vscode alias and extensions

--- flatpak.sh
+++ flatpak.sh
@@ -5,7 +5,15 @@ set -e
 
 # install commonly used apps in a way that works on any distro. 
 sudo pacman -S flatpak
+
 flatpak install flathub com.visualstudio.code
+
+code --install-extension njpwerner.autodocstring
+code --install-extension GitHub.copilot
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+
 flatpak install flathub org.gimp.GIMP
 flatpak install flathub com.discordapp.Discord
 flatpak install flathub org.chromium.Chromium
